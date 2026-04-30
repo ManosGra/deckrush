b4912ab964629c7d9fdad8d543bd8faa20ae658a
ManosGra <manosgrammos9@gmail.com>
2026-04-30 20:05:24 +0300

    update

--- includes/header.php
+++ includes/header.php
@@ -48,7 +48,7 @@
 
   <!-- Custom CSS -->
   <link rel="stylesheet" href="style.css" />
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/android-chrome-512x512.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png" />
   <link rel="apple-touch-icon" href="assets/apple-touch-icon.png" />
 
